d6ca32bae9ff965bf17065814e1c41a336d23f6f
haohan <haohan920@gmail.com>
2023-05-18 10:15:32 +0800

    在寫拿 getallpropducts 的query

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -9,8 +9,31 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ Products, nbHits: Products.length });
 }
 
+//python 表達式
+///queryObject['featured'] = True if featured == 'true' else False
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products testing route" });
+  console.log('req.query',req.query)
+  const featured = req.query.featured
+  const company = req.query.company
+  const name = req.query.name
+  console.log('featured',featured)
+  console.log('company',company)
+  console.log('name',name)
+  // const {featured,company} = req.query
+  //4:16
+  const queryObject= {}
+  if (featured){
+    queryObject.featured = featured === 'true' ? true:false 
+  }
+  if (company){
+    queryObject.company = company
+  }
+  if (name){
+    queryObject.name = name
+  }
+  console.log(queryObject)
+  const products = await Product.find(queryObject)
+  res.status(200).json({ products, nbHits: products.length });
 };
 module.exports = {
   getAllProductsStatic,
